1fabe5bdc2024a732e7861919b8e9291c77e9d8f
Marcos Salvador Hernández Guadarrama <marcoshernandezg18@yahoo.com>
2026-02-21 21:48:46 -0600

    Se agregan instrucciones para actulizar archivos viejos en portal legacy

--- src/app/modules/dashboard/pages/proyectos-legacy/proyectos-legacy.component.ts
+++ src/app/modules/dashboard/pages/proyectos-legacy/proyectos-legacy.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { trigger as animationTrigger, transition, style, animate } from '@angular/animations';
+import { KeycloakService } from 'keycloak-angular';
+import { UtilsService } from 'src/app/modules/shared/services/utils.service';
 
 @Component({
   selector: 'app-proyectos-legacy',
@@ -17,15 +19,45 @@ import { trigger as animationTrigger, transition, style, animate } from '@angula
 })
 export class ProyectosLegacyComponent implements OnInit {
 
+  public puedeVerAyuda: boolean = false;
+
+  isAdmin: any;
+  isUser: any;
+  isPefilApp: any;
+  isPerfilInfraestructura: any;
+
+  @ViewChild('iframeLegacy') iframeElement!: ElementRef;
+
+  // URL del portal legacy
+  private readonly rawUrl = 'http://10.119.79.95:8081/code/html_2025/index.html';
+
   // URL para el portal legacy
   public urlPortalLegacy!: SafeResourceUrl;
 
-  constructor(private sanitizer: DomSanitizer) { }
+  constructor(private sanitizer: DomSanitizer, private util: UtilsService) { }
 
   ngOnInit(): void {
-    const legacyUrl = 'http://10.119.79.95:8081/code/html_2025/index.html';
-    // Sanitizar la URL para que Angular la considere segura
-    this.urlPortalLegacy = this.sanitizer.bypassSecurityTrustResourceUrl(legacyUrl);
+    this.urlPortalLegacy = this.sanitizer.bypassSecurityTrustResourceUrl(this.rawUrl);
+    
+    this.puedeVerAyuda = this.util.isAdmin() || this.util.isPerfilApp() || this.util.isPerfilInfraestructura();
   }
 
+  // Método para recargar el iframe
+  reloadIframe() {
+    try {
+      const iframe = this.iframeElement.nativeElement;
+      // 1. Intentamos obtener la URL actual donde está el usuario navegando
+      let currentUrl = iframe.contentWindow.location.href;
+
+      const urlActual = iframe.src;
+      iframe.src = ''; // Lo vaciamos un milisegundo
+
+      setTimeout(() => {
+        // Al restaurarlo, el navegador busca la página en la que se quedó
+        iframe.src = urlActual;
+      }, 10);
+
+    } catch (e) {
+    }
+  }
 }
